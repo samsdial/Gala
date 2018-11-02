a03bcd5dfe1d55eb13fec751aab918f553090545
samsdial <diastudios@gmail.com>
2018-11-02 09:04:57 -0500

    All Case our javascript

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,14 +1,24 @@
 $(document).ready(function($){
+    var currentPage = 1;
     //menu OPen
     var btnOpenDrop = $('#dropdownMenuLink');
-    btnOpenDrop.on('click', function () {
-        $('#dropdownMenu').addClass('show');
-    });
-
+    var dropmenu    = $('#dropdownMenu');
+    btnOpenDrop.on("click", opening);
     //-- menu open
     var btnOpen = $('#trigger-overlay');
     var btnClose = $('.overlay_close');
     var menu = $('.overlay_contentpush');
+
+    function opening(e) {
+        e.preventDefault();
+        btnOpenDrop.toggleClass('opening');
+        if (dropmenu.hasClass('show')){
+            console.log('Agregar show');
+            dropmenu.removeClass('show');
+        }
+    }
+
+    // OLD
     btnOpen.on('click', function (e) {
         e.preventDefault();
         menu.addClass('open');
@@ -34,4 +44,73 @@ $(document).ready(function($){
         $('#collapseTwo').removeClass('in');
         $('#collapseOne').removeClass('in');
     });
+    //Acctio Form
+    var OpenAction = $('#rvsopen');
+    OpenAction.on('click', function (e) {
+        e.preventDefault();
+        //console.log('Hola Tres')
+        currentPage = 4;
+        changeCurrentPage();
+    });
+    //
+
+    var $section1 = $('#section1');
+    var $section2 = $('#section2');
+    var $section3 = $('#section3');
+    var $section4 = $('#section4');
+    var $section5 = $('#section5');
+    var $section6 = $('#section6');
+    var $section7 = $('#section7');
+    var $section8 = $('#section8');
+    var $section9 = $('#section9');
+    var $section10 = $('#section10');
+
+    changeCurrentPage();
+    function changeCurrentPage()
+    {
+        //console.log('entro');
+        $section1.hide();
+        $section2.hide();
+        $section3.hide();
+        $section4.hide();
+        $section5.hide();
+        $section6.hide();
+        $section7.hide();
+        $section8.hide();
+        $section9.hide();
+        $section10.hide();
+        switch (currentPage) {
+            case 1:
+                $section1.show();
+                break;
+            case 2:
+                $section2.show();
+                break;
+            case 3:
+                $section3.show();
+                break;
+            case 4:
+                $section4.show();
+                break;
+            case 5:
+                $section5.show();
+                break;
+            case 6:
+                $section6.show();
+                break;
+            case 7:
+                $section7.show();
+                break;
+            case 8:
+                $section8.show();
+                break;
+            case 9:
+                $section9.show();
+                break;
+            case 10:
+                $section10.show();
+                break;
+        }
+        currentPage++;
+    }
 });
